6cab59065eb211d9751dda93f8a015e1c76f0f52
Asif Mahmud <r0918559@student.thomasmore.be>
2025-09-18 18:09:41 +0200

    Add MongoDB student collection and functions for data manipulation

--- MongoDB/mongodb_demo.py
+++ MongoDB/mongodb_demo.py
@@ -16,16 +16,49 @@ collection = db["students"]   # create/use a collection called students
 
 # ----- Insert a document -----
 student = {"name": "Asif", "course": ["Data Science"], "graduationDate": datetime(2023, 6, 15)}
+students = [{"name":"Patrick", "age":38, "gpa": 1.5},  
+            {"name":"Sandy", "age":27, "gpa": 4.0},
+            {"name":"Gary", "age":18, "gpa": 2.5}]
+
+# ----- Functions -----
+    
+
+def create_collection():
+    db.create_collection("students")
+
+def drop_collection():
+    collection.drop()
 
 def add_student(student):
     insert_result = collection.insert_one(student)
     print("Inserted document ID:", insert_result.inserted_id)
 
+def insert_multiple(students):
+    insert_result = collection.insert_many(students)
+    print("Inserted document IDs:", insert_result.inserted_ids)
+
 def find_student(name):
-  print("document:", collection.find({"name": "Asif"}))
+  results = collection.find({"name": name})
+  for doc in results:
+      print(doc)
 
 def all_students():
-    print("All documents:")
     for doc in collection.find():
         print(doc)
+
+# ----- Usage -----
+# open_connecton()
+# create_collection()
+# drop_collection()
+# insert_multiple(students)
+# add_student(student)
+# find_student("Asif")
+# all_students()
+# client.close()
+
+
+
+
+
+
 
